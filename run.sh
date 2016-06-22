7ae5786af66033943a3de3aeca05068f142d0508
Asoul <b00902036@ntu.edu.tw>
2016-06-22 17:12:31 +0800

    daily update

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ cd /home/asoul/tsec/
 /usr/bin/git add .
 /usr/bin/git commit -m "daily update"
 /usr/bin/git push
+/usr/bin/git fetch --depth=1
+/usr/bin/git reflog expire --expire-unreachable=now --all
+/usr/bin/git gc --aggressive --prune=all
